aa0b396fe07633232ea5c1a83d7dbc51197abb1f
Srijana Tamang <srijana.tmng7@gmail.com>
2019-02-26 20:02:41 -0500

    First commit for movie app codepath

---
+++ app/src/main/java/com/codepath/flickster/Model/Movie.java
@@ -0,0 +1,47 @@
+package com.codepath.flickster.Model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String overview;
+    String title;
+    String backdropPath;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title=jsonObject.getString("title");
+        overview=jsonObject.getString("overview");
+        backdropPath=jsonObject.getString("backdrop_path");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for(int i=0;i<movieJsonArray.length();i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+
+        }
+        return movies;
+
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+    }
+    public String getBackdropPathPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ app/src/main/java/com/codepath/flickster/adapters/MoviesAdapter.java
@@ -0,0 +1,79 @@
+package com.codepath.flickster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.flickster.Model.Movie;
+import com.codepath.flickster.R;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("smile","OnCreateViewHolder");
+        View view=LayoutInflater.from(context).inflate(R.layout.item_movie,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("smile","OnBindViewHolder"+position);
+
+        Movie movie=movies.get(position);
+    //bind the movie data into view holder
+        holder.bind(movie);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvTitle=itemView.findViewById(R.id.tvTitle);
+            tvOverview=itemView.findViewById(R.id.tvOverview);
+            ivPoster=itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl= movie.getPosterPath();
+            if(context.getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE)
+                imageUrl=movie.getBackdropPathPath();
+            Glide.with(context).load(imageUrl).into(ivPoster);
+        }
+    }
+}
